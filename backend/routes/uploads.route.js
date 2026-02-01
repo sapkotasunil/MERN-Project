9f681aaaf3a0fb2431fc2c91348d4f7f540e0486
Sunil Sapkota <sunil402sapkota@gmail.com>
2026-02-01 12:17:48 +0545

    add image upload functionality with Cloudinary and multer integration

---
+++ backend/routes/uploads.route.js
@@ -0,0 +1,51 @@
+import express from "express";
+import multer from "multer";
+import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLODINARY_API_NAME,
+  api_key: CLODINARY_API_KEY,
+  api_secret: CLODINARY_API_SECRET,
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const fileName = Date.now() + path.extname(file.originalname);
+    cb(null, fileName);
+  },
+});
+
+const imageFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("only image file allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter: imageFilter,
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+const router = express.Router();
+router.post("/", upload.single("image"), async (req, res) => {
+  try {
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: "ecomm",
+    });
+    res.send({
+      message: "Image uploaded successfully!",
+      image: result.secure_url,
+    });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+export default router;
